875d3c3e704fd3fdf7edd0f62a2290e5dcc3d403
hello-liang <liangshuaibing@outlook.com>
2022-01-27 01:14:30 +0800

    remove some error folder and pso algorithm

--- src/5-Contexts/ls_loss/prototxt/run_Ls_loss.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-TOOLS=../../build/tools
-
-GLOG_logtostderr=1 $TOOLS/caffe train -solver solver_Ls_loss.prototxt  
-echo 'Done.'
--- src/UCF101/lq_loss/prototxt/run_Lq_loss.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-TOOLS=../../build/tools
-
-GLOG_logtostderr=1 $TOOLS/caffe train -solver solver_Lq_loss.prototxt 
-echo 'Done.'
